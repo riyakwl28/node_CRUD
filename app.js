9981df93d5f794ea99aa2564f6bcac550157ebe1
Riya Khandelwal <38571771+riyakwl28@users.noreply.github.com>
2024-01-16 15:23:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+//here
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
